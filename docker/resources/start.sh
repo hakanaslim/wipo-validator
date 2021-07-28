1e6c941b62b819fb6fa4de777a771d5f1a2fca61
PP94150 <ppinta.external@epo.org>
2021-07-28 19:45:16 +0300

    wipo validator

--- docker/resources/start.sh
+++ docker/resources/start.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 _JVM_DEBUG='-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:8001'
-_JVM_PROXY_SETTINGS='-Dhttp.proxyHost=proxylb.internal.epo.org -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxylb.internal.epo.org -Dhttps.proxyPort=8080 -Dhttp.nonProxyHosts=localhost|127.0.0.1|*.local|*.internal.epo.org'
+_JVM_PROXY_SETTINGS='-Dhttp.proxyHost=proxylb.internal.epo.org -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxylb.internal.epo.org -Dhttps.proxyPort=8080 -Dhttp.nonProxyHosts=localhost|127.0
 _JVM_ARGS=''
 _JVM_SECURITY=''
 
