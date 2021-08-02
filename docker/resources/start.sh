e6f538922a08140347863e4ea5b4f4e99112eb83
PP94150 <ppinta.external@epo.org>
2021-08-02 15:11:53 +0300

    wipo validator

--- docker/resources/start.sh
+++ docker/resources/start.sh
@@ -1,6 +1,12 @@
 #! /bin/sh
 
-_JVM_DEBUG='-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:8001'
-_JVM_PROXY_SETTINGS='-Dhttp.proxyHost=proxylb.internal.epo.org -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxylb.internal.epo.org -Dhttps.proxyPort=8080 -Dhttp.nonProxyHosts=localhost|127.0
+#_JVM_DEBUG='-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:8001'
+#_JVM_PROXY_SETTINGS='-Dhttp.proxyHost=proxylb.internal.epo.org -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxylb.internal.epo.org -Dhttps.proxyPort=8080 -Dhttp.nonProxyHosts=localhost|127.0
+
+#_JVM_PROTOCOL='-Dsun.net.http.allowRestrictedHeaders=true'
+#_JVM_DEBUG='-agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n'
+_JVM_SECURITY='-Djava.security.auth.login.config=/etc/krb/login.conf -Djava.security.krb5.conf=/etc/krb/krb5.conf -Dorg.epo.security.protocol.wrapper.http.url2spn-mapping-file=/etc/krb/url-to-spn-mappings.properties'
+_JVM_PROXY_SETTINGS='-Dhttp.proxyHost=proxylb.internal.epo.org -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxylb.internal.epo.org -Dhttps.proxyPort=8080 -Dhttp.nonProxyHosts=localhost|127.0.0.1|*.local|*.internal.epo.org'
+_JVM_TIMEZONE='-Duser.timezone=Europe/Amsterdam'
 
 /usr/bin/java $*
