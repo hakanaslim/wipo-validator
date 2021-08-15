19d916f0b1a4f07827a00adba2cc9868b99bbfd1
PP94150 <ppinta.external@epo.org>
2021-08-15 19:24:15 +0300

    ambasador update

--- docker/resources/start.sh
+++ docker/resources/start.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-#_JVM_DEBUG='-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:8001'
+_JVM_DEBUG='-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:8001'
 #_JVM_PROXY_SETTINGS='-Dhttp.proxyHost=proxylb.internal.epo.org -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxylb.internal.epo.org -Dhttps.proxyPort=8080 -Dhttp.nonProxyHosts=localhost|127.0
 
 #_JVM_PROTOCOL='-Dsun.net.http.allowRestrictedHeaders=true'
